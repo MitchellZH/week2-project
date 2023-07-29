ac2600b30c49a3c70f9c2e7b0f85d3a433891d14
Mitchell Hamm <www.zanehamm@yahoo.com>
2023-07-29 13:26:28 -0700

    built basic stucture of program

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,1 +1,21 @@
 # Start Your Code here
+class parkingGarage():
+    def __init__(self):
+        self.tickets = ["ticket", "ticket", "ticket", "ticket", "ticket"],
+        self.parkingSpaces = ["space", "space", "space", "space", "space"],
+        self.currentTicke = {"paid": False}
+    
+    def takeTicket():
+        pass
+    
+    def payForParking():
+        pass
+    
+    def leaveGarage():
+        pass
+    
+    def runner():
+        pass
+
+
+    print("Thank You, have a nice day")
